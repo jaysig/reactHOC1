9796877dcef912c691a81e59dea1e0297496b243
Jaysig <jasontsigmon@gmail.com>
2016-11-01 11:50:14 -0500

    Added the comment box

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,31 +1,31 @@
 import { renderComponent , expect } from '../test_helper';
 import App from '../../src/components/app';
 
-// describe('App' , () => {
-//   let component;
-//
-//   beforeEach(() => {
-//     component = renderComponent(App);
-//   });
-//
-//   it('renders something', () => {
-//     expect(component).to.exist;
-//   });
-// });
+describe('App' , () => {
+  let component;
 
-//Use describe to group together similar tests
-describe('App', () => {
-
-  // Use 'it' to test a single attribute of a target
-  it('shows the correct text', () => {
-
-    //create an instance of App
-    const component = renderComponent(App);
-
-
-    // Use 'expect' to make an 'assertion' about a target
-    expect(component).to.contain('React simple starter');
-
+  beforeEach(() => {
+    component = renderComponent(App);
   });
 
+  it('shows a comment box', () => {
+    expect(component.find('.comment-box')).to.exist;
+  });
 });
+
+// //Use describe to group together similar tests
+// describe('App', () => {
+//
+//   // Use 'it' to test a single attribute of a target
+//   it('shows the correct text', () => {
+//
+//     //create an instance of App
+//     const component = renderComponent(App);
+//
+//
+//     // Use 'expect' to make an 'assertion' about a target
+//     expect(component).to.contain('React simple starter');
+//
+//   });
+//
+// });
--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -2,21 +2,42 @@ import { renderComponent , expect } from '../test_helper';
 import CommentBox from '../../src/components/comment_box';
 
 describe('CommentBox', () => {
+  let component
+  beforeEach(() => {
+    component = renderComponent(CommentBox);
+  });
+
   it('has the correct class', () => {
-    const component = renderComponent(CommentBox);
+
     expect(component).to.have.class('comment-box');
 
   });
   it('has a text area', () => {
-    const component = renderComponent(CommentBox);
     expect(component.find('textarea')).to.exist;
 
   });
 
   it('has a button', () => {
-    const component = renderComponent(CommentBox);
-    expect(component.find('button')).to.existl
+    expect(component.find('button')).to.exist;
 
   });
+
+  describe('entering some text', () => {
+    beforeEach(() => {
+      //Simulates fake events
+      component.find('textarea').simulate('change', 'new comment');
+    });
+
+    it('shows that text shows in the textarea', () => {
+      expect(component.find('textarea')).to.have.value('new comment');
+
+    });
+
+    it('when submitted, clears the input', () => {
+      expect(component.find('button')).to.exist;
+
+    });
+  })
+
 
 });

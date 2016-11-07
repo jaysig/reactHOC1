dea6aba1e39129495b84a7321f3c8da8d00d1ebc
Jaysig <jasontsigmon@gmail.com>
2016-11-07 13:46:35 -0600

    comment list tests pass

--- src/components/comment_list.js
+++ src/components/comment_list.js
@@ -5,7 +5,7 @@ const CommentList = (props) => {
   const list = props.comments.map(comment => <li key={comment}>{comment}</li>)
   return (
     <ul className="comment-list">
-
+      {list}
     </ul>
   )
 }

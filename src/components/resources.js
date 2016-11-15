cfd0cf9d20b02da95524d623ff820a87f324159a
Jaysig <jasontsigmon@gmail.com>
2016-11-15 12:46:41 -0600

    Added authenication action and reducer

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+    Super
+      <ul>
+        <li>1 cup sugar</li>
+        <li>1 cup pe</li>
+        <li>1 cup salt</li>
+      </ul>
+    </div>
+  )
+}

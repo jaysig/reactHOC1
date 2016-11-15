895695f11581b25e2c3d98c0772d7352cdc1f008
Jaysig <jasontsigmon@gmail.com>
2016-11-15 12:55:06 -0600

    Made the header into a connected container

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class Header extends Component {
   authButton() {
+    if (this.props.authenticated) {
+      return <button onClick={() => this.props.authenticate(false)}>Sign Out</button>
+    } else {
+      return <button onClick={() => this.props.authenticate(true)}> Sign In</button>
+    }
     return <button>Toogle Auth </button>;
   }
 
@@ -24,4 +31,7 @@ class Header extends Component {
     )
   }
 }
-export default Header
+function mapStateToProps(state) {
+  return { authenticated: state.authenticated };
+}
+export default connect(mapStateToProps, actions)(Header)
